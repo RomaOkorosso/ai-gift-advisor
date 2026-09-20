136c416aa0fab3a4491b613a90f67076cc29e0ea
RomaOkorosso <zatrutin.roma.work@gmail.com>
2026-09-20 19:22:50 +0300

    add testing fixture is rollbacked

--- tests/test_database.py
+++ tests/test_database.py
@@ -24,3 +24,33 @@ async def test_create_gift(db_session):
 
     assert saved_gift.budget == 3000
     assert saved_gift.description == "Test gift description"
+
+
+@pytest.mark.anyio
+async def test_1_create_gift(db_session):
+    gift = GiftRequestModel(
+        budget=777777,
+        description="Transaction rollback test",
+    )
+
+    db_session.add(gift)
+    await db_session.commit()
+
+    result = await db_session.execute(
+        select(GiftRequestModel).where(
+            GiftRequestModel.budget == 777777
+        )
+    )
+
+    assert result.scalar_one().budget == 777777
+
+
+@pytest.mark.anyio
+async def test_2_gift_was_rolled_back(db_session):
+    result = await db_session.execute(
+        select(GiftRequestModel).where(
+            GiftRequestModel.budget == 777777
+        )
+    )
+
+    assert result.scalar_one_or_none() is None
