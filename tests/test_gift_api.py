2baee60e33441df5e47b0a91a5b0c18b85d05e2c
RomaOkorosso <zatrutin.roma.work@gmail.com>
2026-09-24 09:24:19 +0300

    fix depricated style

--- tests/test_gift_api.py
+++ tests/test_gift_api.py
@@ -1,4 +1,6 @@
-from fastapi.testclient import TestClient
+import pytest
+
+from httpx import ASGITransport, AsyncClient
 
 from app.api.dependencies import get_gift_service
 from app.main import app
@@ -17,25 +19,28 @@ def override_gift_service():
     return FakeGiftService()
 
 
-def test_create_gift():
+@pytest.mark.anyio
+async def test_create_gift():
     app.dependency_overrides[get_gift_service] = override_gift_service
 
     try:
-        with TestClient(app) as client:
-            response = client.post(
+        async with AsyncClient(
+                transport=ASGITransport(app=app),
+                base_url="http://test",
+        ) as client:
+            response = await client.post(
                 "/gift",
                 json={
                     "budget": 1000,
                     "description": "Друг любит программирование",
                 },
             )
-
-        assert response.status_code == 201
-        assert response.json() == {
-            "id": 42,
-            "budget": 1000,
-            "description": "Друг любит программирование",
-        }
-
     finally:
-        app.dependency_overrides.clear()
+        app.dependency_overrides.pop(get_gift_service, None)
+
+    assert response.status_code == 201
+    assert response.json() == {
+        "id": 42,
+        "budget": 1000,
+        "description": "Друг любит программирование",
+    }
